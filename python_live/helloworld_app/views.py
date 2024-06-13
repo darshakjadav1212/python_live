af45f9851d1d65fbfe94d32366c7a28dd763e68a
Darshak Jadav <darshakjadav.1212.2024@gmail.com>
2024-06-13 18:06:03 +0530

    final commit

---
+++ python_live/helloworld_app/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+
+from django.http import HttpResponse
+
+# Create your views here.
+
+def sayHello(request):
+    return HttpResponse("<h1 style = 'color:red'>Hello World! Jay Swaminaryan</h1>")
